329bc3f59de53faea8f7bf0760354995f4f1cce1
Gianluca Pettenon <115878175+Giany74@users.noreply.github.com>
2023-10-08 11:42:35 +0200

    findById

--- routes/api/anuncios.js
+++ routes/api/anuncios.js
@@ -2,14 +2,31 @@ const express = require('express');
 const router = express.Router();
 const Anuncio = require('../../models/Anuncio');
 
+// GET api/anuncios -> devuelve lista de agentes
 router.get('/', async (req, res, next) => {
 
     try {
-        const anuncios = await Anuncio.find(); 
+        const anuncios = await Anuncio.find();
+
+        // throw new Error('fallo forzado');
+
         res.json({ results: anuncios }) 
     } catch (err) {
         next(err);
     }
 });
+
+// GET /api/anuncios/(_id)
+router.get('/:id', async (req,res,next) => {
+    try {
+        const id = req.params.id;
+
+        const anuncio = await Anuncio.findById(id);
+
+        res.json({ result: anuncio});
+    } catch (err) {
+        next(err);
+    }
+})
 
 module.exports = router
