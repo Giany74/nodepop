d9791d9e72a67b129a88fb2917576f5c19798339
Gianluca Pettenon <115878175+Giany74@users.noreply.github.com>
2023-10-08 21:36:49 +0200

    Anuncio last

--- models/Anuncio.js
+++ models/Anuncio.js
@@ -1,13 +1,31 @@
 const mongoose = require('mongoose');
 
 const anuncioSchema = mongoose.Schema({
-    nombre: String,
-    venta: Boolean,
-    precio: Number,
+    name: { type: String, index: true },
+    buySell: { type: String, index: true },
+    price: { type: String },
     foto: String,
-    tags: Array
+    tags: { type: [String], index: true }
+}, {
+    // collection: 'anuncios' // para forzar un nombre concreto de colección
 });
+
+// Tipos de métodos:
+//  - Estático: método que está en el modelo (p.e. Agente.lista())
+//  - De instancia: método que tienen las instancias (p.e. agente.saluda())
+anuncioSchema.statics.lista = function(filtro, skip, limit, fields) {
+  const query = Anuncio.find(filtro);
+  query.skip(skip);
+  query.limit(limit);
+  query.select(fields);
+
+  return query.exec();
+}
 
 const Anuncio = mongoose.model('Anuncio', anuncioSchema);
 
 module.exports = Anuncio;
+
+
+
+
