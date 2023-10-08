0affa7aeb683e28ebbe15bd84baa7a9d8547c696
Gianluca Pettenon <115878175+Giany74@users.noreply.github.com>
2023-10-08 21:38:13 +0200

    routes update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+// Ruta para la página inicial
+router.get('/', async (req, res) => {
+  try {
+    const response = await axios.get('http://127.0.0.1:3000/api/anuncios'); // Reemplaza con la URL correcta de la API
+    console.log(response.data)
+    const data = response.data; // Obtiene los datos de la respuesta
+
+    res.render('index', { data: data }); // Pasa los datos al archivo index.ejs
+  } catch (error) {
+    console.error('Error al obtener los datos de la API:', error);
+    res.status(500).send('Error al obtener los datos de la API');
+  }
 });
 
 module.exports = router;
